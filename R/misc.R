aad564422c3ac91683afdb2ccd0dad399bc437cf
mwelz <welz@ese.eur.nl>
2021-11-23 18:55:51 +0100

    Small bugfix in GenericML_single() when clusters are passed

--- R/misc.R
+++ R/misc.R
@@ -248,8 +248,8 @@ GenericML_single <- function(Z, D, Y,
                             X1_BLP                     = X1_BLP,
                             X1_GATES                   = X1_GATES,
                             HT                         = HT,
-                            vcov_BLP                   = vcov_BLP,
-                            vcov_GATES                 = vcov_GATES,
+                            vcov_BLP                   = setup_vcov_align(vcov_BLP),   # align for consistency
+                            vcov_GATES                 = setup_vcov_align(vcov_GATES), # align for consistency
                             equal_variances_CLAN       = equal_variances_CLAN,
                             quantile_cutoffs           = quantile_cutoffs,
                             diff_GATES                 = diff_GATES,
